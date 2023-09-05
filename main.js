0f41a7875d44af09a12a5c2fd435818554c1d134
sidaxe pro <108492494+hoosid@users.noreply.github.com>
2023-09-05 13:13:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
+// up
+let span = document.querySelector(".up");
+window.onscroll = function () {
+	console.log(this.scrollY)
+	if (this.scrollY >= 600) {
+		span.classList.add("show");
+	} else {
+		span.classList.remove("show");
+	}
+};
+
+span.onclick = function () {
+	window.scrollTo({
+		top: 0,
+		behavior: "smooth",
+	});
+};
+
 window.addEventListener('scroll',reveal);
 function reveal(){
 	var reveals =document.querySelectorAll('.reveal');
@@ -21,3 +39,4 @@ myButton.addEventListener('click', function() {
 
   alert('Thanks for donnation');
 });
+
